7bc80284ed03dac1ca29b3ebcaeaa3b1af87a618
John Roberts <john.calvin.roberts@gmail.com>
2023-12-03 14:29:15 -0500

    day 3 part 2

--- day/03/challenge.py
+++ day/03/challenge.py
@@ -18,6 +18,27 @@ def is_invalid_adjacent(row, prev_row, next_row, start, end):
     return False
 
 
+def compose_string_chunk_with_numbers_in_tact(row: str | None, idx: int):
+    if row is None:
+        return "..."
+
+    def find_sequence_boundaries(s, index, forward=True):
+        while index >= 0 and index < len(s) and s[index].isdigit():
+            index += 1 if forward else -1
+        return index + (0 if forward else 1)
+
+    start_idx = find_sequence_boundaries(row, idx - 1, forward=False)
+    end_idx = find_sequence_boundaries(row, idx + 1, forward=True)
+
+    chunk = row[start_idx:end_idx]
+
+    # Ensuring that the chunk is at least 3 characters long
+    if len(chunk) < 3:
+        chunk = chunk.rjust(3, ".")[:3]
+
+    return chunk
+
+
 # Find all the part numbers. Part numbers are NUMBERS that are surrounded by a symbol.
 # A char is a symbol if it's not a . or a number.
 # Sum all the part numbers
@@ -41,8 +62,28 @@ def part1(input_str: str):
     return sum(part_numbers)
 
 
+# Any "*" that touches two numbers is a gear ratio, get the value by multiplying the two numbers it touches
+# return the sum of all gear ratios
 def part2(input_str: str):
-    pass
+    schematic_rows = input_str.split("\n")
+    gear_ratios: list[int] = []
+    for index, row in enumerate(schematic_rows):
+        prev_row = schematic_rows[index - 1] if index > 0 else None
+        next_row = (
+            schematic_rows[index + 1] if index < len(schematic_rows) - 1 else None
+        )
+        for c_idx, char in enumerate(row):
+            if char == "*":
+                curr_string = compose_string_chunk_with_numbers_in_tact(row, c_idx)
+                prev_string = compose_string_chunk_with_numbers_in_tact(prev_row, c_idx)
+                next_string = compose_string_chunk_with_numbers_in_tact(next_row, c_idx)
+                numbers = []
+                for span in [prev_string, curr_string, next_string]:
+                    susbtring_numbers = [int(num) for num in re.findall(r"\d+", span)]
+                    numbers.extend(susbtring_numbers)
+                if len(numbers) >= 2:
+                    gear_ratios.append(numbers[0] * numbers[1])
+    return sum(gear_ratios)
 
 
 if __name__ == "__main__":
@@ -57,9 +98,9 @@ if __name__ == "__main__":
 ...$.*....
 .664.598.."""
     result = part1(fixture)
-    print(result)
     expected = 4361
     assert result == expected
     expected2 = 467835
     result2 = part2(fixture)
+    print(result2)
     assert result2 == expected2
